9845823059f9682b1256c686f5a4d87abe92d5c7
MacStudent <macstudent@126-imac16.local>
2019-02-13 18:36:59 -0500

    class and structure of ! and ?

--- Day 4/main.swift
+++ Day 4/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+
+class Person
+{
+    var pid: Int=0
+    var fname:String!
+    var lname:String?
+    
+    
+    func setData()
+    {
+        fname = "Mandeep"
+        lname = "Kaur"
+    }
+    func display()
+    {
+        print(pid)
+        //print(fname, lname!)
+        //let l = lname ?? "Kaur"
+        
+        if let f = fname, let l = lname
+        {
+        let s = f + " " + l
+        print(s)
+    }
+    }
+}
+ var p1 = Person()
+
+p1.setData()
+p1.display()
 
